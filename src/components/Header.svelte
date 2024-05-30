684ec619d88aab2b6d062b6f28fedeef1fd80412
Jim <jimhalpertgoodbunny@gmail.com>
2024-05-30 19:03:16 -0400

    3rd

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,7 +5,19 @@
 <header class="flex flex-col relative z-20">
     <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
         <a href="/">
-        <h1 class="font-semibold"><span class="text-indigo-600">AI</span> Girlfriend Creator</h1></a>
+        <h1 class="font-semibold"><span class="text-indigo-600">AI</span> Girlfriend Creator</h1>
+    </a>
+    <button class="md:hidden grid place-items-center">
+        <i class="fa-solid fa-bars"></i>
+    </button>
+    <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+        <a class="duration-200 hover:text-pink-500 cursor-pointer" href="/">Pricing</a>
+        <a class="duration-200 hover:text-pink-500 cursor-pointer" href="/">More AI Tools</a>
+        <a class="duration-200 hover:text-pink-500 cursor-pointer" href="/">Become a new Husband</a>
+        <button class="specialBtn">
+            <p>Create your AI Girlfriend for free!</p>
+        </button>
+    </nav>
     </div>
 
 </header>
