563440d9789c8da618dd0cdd2d873f2442850e01
Jim <jimhalpertgoodbunny@gmail.com>
2024-05-30 18:52:35 -0400

    2nd

---
+++ src/components/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<header class="flex flex-col relative z-20">
+    <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
+        <a href="/">
+        <h1 class="font-semibold"><span class="text-indigo-600">AI</span> Girlfriend Creator</h1></a>
+    </div>
+
+</header>
