e247bd7c2cd24cb7904016b94e70b46371b3b44c
Jim <jimhalpertgoodbunny@gmail.com>
2024-05-30 21:52:33 -0400

    4th

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,7 +2,12 @@
     import Header from "./Header.svelte";
 	import SectionWrapper from "./SectionWrapper.svelte";
 </script>
+
 <SectionWrapper>
     <Header />
-
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center">
+            <span class="text-violet-400">Girlfriend</span> Customizer <span class="text-slate-600">AI</span>
+        </h2>
+    </div>
 </SectionWrapper>
